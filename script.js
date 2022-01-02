8a9e8062ac5df99ec96ce1c7f06f2a57c898844e
Marcosta23 <mpc2073@yahoo.com.br>
2022-01-02 20:59:59 -0300

    dataset

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firdtCard, secondCard;
 
 function flipCard() {
-this.classList.toggle('flip');
+this.classList.add('flip');
+if(!hasFlippedCard){
+    hasFlippedCard = true;
+    firdtCard = this;
+    return;
+}
+secondCard = this;
+checkForMath();
 };
+
 
 cards.forEach((card) =>{
     card.addEventListener('click', flipCard)
